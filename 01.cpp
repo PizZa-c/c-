a2ad821b293cb1d74850fb06b0128ea30164b59a
PizZa-c <1554802723@qq.com>
2025-03-02 19:41:27 +0800

    Create 01.cpp
    c++prime 第一章习题

---
+++ 01.cpp
@@ -0,0 +1,92 @@
+#include<iostream>
+using namespace std;
+int main()
+{	
+	//int x = 2, y = 4;
+	//cout << x * y << endl;//练习1.4用乘法输入结果
+
+
+
+	//cout << "Hello";
+	//cout << ", "; 
+	//cout << "World";
+	//cout << endl;  //1.5练习分开打印
+
+
+
+	//int x = 50, sum = 0;
+	//while (x <= 100)
+	//{
+	//	sum +=x ;
+	//	x++;
+	//}
+	//cout << sum << endl;//练习1.9用while将50-100整数相加
+
+
+	//int x = 10;
+	//int sum = 0;
+	//while (x >= 0)
+	//{
+	//	sum += x;
+	//	cout << x << endl;//练习1.10  输出10-0整数
+	//	--x;
+	//}
+	//
+
+	//int x = 0, y = 0;
+	//cin >> x >> y;
+	//while (x != y)
+	//{
+	//	if (x > y)
+	//	{
+	//		x--;
+	//		if (x == y)break;
+	//		cout << x << " ";			
+	//	}
+	//	else
+	//	{
+	//		y--;
+	//		if (x == y)break;
+	//		cout << y << " ";
+	//	}
+	//	
+	//}
+	//cout << endl;//练习1.11  打印两个整数范围内的所有整数
+
+	//int sum = 0;
+	//for (int i = 50; i <= 100; i++)
+	//{
+	//	sum += i;	
+	//}
+	////cout << sum << endl;
+	//for (int i = 10; i > 1;)
+	//	cout << --i << endl;//用递减输出10-1之间的整数
+	//int x =0, y = 0;
+	//cin >> x >> y;
+	//for (100 > x > 10; 10 < y < 100;)
+	//{
+	//	if (x+1 == y || 1+y == x)break;
+	//	if (x > y)
+	//	{
+	//		cout << ++y << " ";
+	//	}
+	//	else 
+	//	{
+	//		cout << ++x << " ";
+	//	}
+	// 
+	//}//输出两个数之间的整数
+	//上述内容为1.13习题用for循环重新完成练习
+
+
+	//int x = 0, sum = 0;
+	//while (cin >> x)
+	//{
+	//	sum += x;
+	//	cout << sum << " ";
+	//}//练习1.16从cub读取一组数 输出和
+
+
+	system("pause");
+	return 0;
+}
