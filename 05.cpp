03fd70756a5d886a46f65526f16c08b531b2b260
PizZa-c <1554802723@qq.com>
2025-03-18 16:00:36 +0800

    Create 05.cpp

---
+++ 05.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+int main()
+{
+	/*int i = 1;
+	int* p = &i;
+	vector<int*> vec(*p);
+	if (*vec.begin() == *((vec.begin)()))cout << "1";
+	if (*vec.begin() + 1 == ((*((vec.begin)())) + 1))cout << "1";*/
+	//Á·Ï°4.2
+	//int i = ((12 / 3) * 4) + (5 * 15) + ((24 % 4) / 2);
+	////if (i == 12 / 3 * 4 + 5 * 15 + 24 % 4 / 2)cout<<i;
+	//while (cin >> i)
+	//{
+	//	if (i % 2)cout << "ÊÇÆæÊý" << endl;
+	//	else
+	//	{
+	//		cout << "ÊÇÅ¼Êý" << endl;
+	//	}
+	//}//4.6ÅÐ¶ÏÆæÅ¼
+	/*int i = 0, j = 4, k = 3, c = 99;
+	c= i!= j <k;
+	cout << c;*/
+	//4.12
+
+	//int i; double d;
+	////d = i = 3.5; cout << d;
+	//int* pi =&i;
+	//i = 3;
+	//*pi =i=0; cout << *pi;
+
+
+
+	return 0;
+}
